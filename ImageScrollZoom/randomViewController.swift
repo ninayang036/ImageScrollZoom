d6ddc2254095e8792c81fce6970c3fe0dde5cf0a
Nina Yang <nina39490@gmail.com>
2021-05-05 14:27:39 +0800

    First edition

--- ImageScrollZoom/randomViewController.swift
+++ ImageScrollZoom/randomViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  randomViewController.swift
 //  ImageScrollZoom
 //
 //  Created by Yang Nina on 2021/5/4.
@@ -7,21 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class randomViewController: UIViewController {
 
     @IBOutlet weak var scrollview: UIScrollView!
     @IBOutlet weak var pagecontrol: UIPageControl!
+    @IBOutlet var imageviews: [UIImageView]!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        UIUpdate()
     }
-    
     @IBAction func pageChange(_ sender: UIPageControl) {
         let point = CGPoint(x: scrollview.bounds.width*CGFloat(sender.currentPage), y: 0)
         scrollview.setContentOffset(point, animated: true)
     }
+    @objc func UIUpdate(){
+        for imageView in imageviews {
+            //隨機圖片網址
+            let str = "https://picsum.photos/455/774"
+            if let url = URL(string: str){
+                URLSession.shared.dataTask(with: url){ (data , response ,error) in
+                    if let date = data,
+                       let image = UIImage(data: date){
+                           DispatchQueue.main.async {
+                           imageView.image = image
+                         }
+                    }
+                }.resume()
+            }
+        }
+    }
 }
-extension ViewController:UIScrollViewDelegate{
+extension randomViewController:UIScrollViewDelegate{
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
         let page = scrollView.contentOffset.x/scrollView.bounds.width
         pagecontrol.currentPage = Int(page)
@@ -35,4 +51,3 @@ extension ViewController:UIScrollViewDelegate{
         return nil
     }
 }
-
